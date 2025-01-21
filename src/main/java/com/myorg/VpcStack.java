e476942405b36ecd381d4e0a84d24ffed66e992c
FlavioFCarvalho <flaviocfr@gmail.com>
2025-01-21 10:13:26 -0300

    Criação do cluster

--- src/main/java/com/myorg/VpcStack.java
+++ src/main/java/com/myorg/VpcStack.java
@@ -8,6 +8,8 @@ import software.constructs.Construct;
 
 public class VpcStack extends Stack {
 
+    private Vpc vpc;
+
     public VpcStack(final Construct scope, final String id){
         this(scope,id, null);
 
@@ -16,8 +18,12 @@ public class VpcStack extends Stack {
     public VpcStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        Vpc.Builder.create(this, "Vpc01")
+      vpc =  Vpc.Builder.create(this, "Vpc01")
                 .maxAzs(3)
                 .build();
+    }
+
+    public Vpc getVpc(){
+        return  vpc;
     }
 }
